a63545b9369f6fedef1f9b5fc08eb744cd0df350
Jerry Navarro <jerry.navarro@asu.edu>
2020-09-14 12:55:21 -0700

    update

--- main.cpp
+++ main.cpp
@@ -17,10 +17,14 @@ void shift(string &s, int n){
 int main(int argc, char* argv[]) {
     string textFileInput;
 
-    if(argc == 1){cout << "please enter ./run argument\n" << "[arguments: insertion, quick]\n"; return 0;}
+    if(argc == 1){cout << "please enter ./run argument\n" << "[argument: insertion, quick]\n"; return 0;}
 
 // if argument = insertion
     if(string(argv[1]) == "insertion" && argc == 2){
+        /*
+         * we use while loop for get line, so everytime there is a new line, it will loop through the program.
+         * the loop will terminate when it reaches the last delimiter
+        */
         while(getline(cin, textFileInput)) {
 
             int sentenceSize = textFileInput.length();
@@ -30,6 +34,7 @@ int main(int argc, char* argv[]) {
              * populate the array sentence[i++] with each left cyclic shift
              * the population will stop when sentenceSize is reached,
              * which will be 1 shift before getting original sentence
+             * everytime we loop, the newly shifted string will be in sentence[i+1]
              * */
             for (int i = 0; i < sentenceSize; i++) {
                 sentence[i] = textFileInput;
@@ -53,6 +58,16 @@ int main(int argc, char* argv[]) {
                 }
                 sentence[j + 1] = temp;
             }
+
+            /*
+             * This is to create last string of all chars at the end of the array list sentence[i]
+             * I used sentence[i][sentenceSize - 1] because this is the end element of each i, from 0 to sentenceSize
+             * */
+            string lastString[sentenceSize];
+            for (int i = 0; i < sentenceSize; i++) {
+                lastString[i] = sentence[i][sentenceSize - 1];
+            }
+
             /*
              * This is my for loop that detects and outputs which element
              * of sentence[i] has the original string.
@@ -60,24 +75,18 @@ int main(int argc, char* argv[]) {
              *  string from sentence[i] is the same as
              * textFileInput, we will output the virtual element number i.
              * Will try to create a function originalStringLocation(arg 1, arg2)
+             * added the break function so when the index is found, we exit the for loop
              * */
             for (int i = 0; i < sentenceSize; i++) {
                 if (sentence[i] == textFileInput) {
                     cout << i << endl;
+                    break;
                 }
             }
-            /*
-             * This is to create last string of all chars at the end of the array list sentence[i]
-             * I used sentence[i][sentenceSize - 1] because this is the end element of each i, from 0 to sentenceSize
-             *
-             * */
-            string lastString[sentenceSize];
-            for (int i = 0; i < sentenceSize; i++) {
-                lastString[i] = sentence[i][sentenceSize - 1];
-            }
+
             /*
              * This function was a bit confusing to make, but hopefully I can clear things up.
-             * the for loop i, starts at 0. we want to compare element 0 to element 1
+             * In the for loop, i starts at 0. we want to compare element i to element i+1
              * we initialize count back to 1 for the arithmetic in the while loop.
              * we only reach the while loop if the comparison of i+1 and i are the same.
              * if they are the same, we will i++ and count++ to count how many elements have the same characters
@@ -95,11 +104,10 @@ int main(int argc, char* argv[]) {
              * out output would be:
              * 5X
              * since we are incrementing by 1 each time in the while loop, by the time we exit the while loop,
-             * we will know when the last element was the same letter, and proceed to count the next elements after.
+             * we will know when the last element was the same letter, and proceed to count from the next letter after.
              * in the case we dont enter the while loop, our count will always be 1 because of the letter being read
              * at least once.
                 */
-            int clusters = 1;
             for (int i = 0; i < sentenceSize; i++) {
                 int count = 1;
                 while (lastString[i + 1] == lastString[i]) {
@@ -107,19 +115,8 @@ int main(int argc, char* argv[]) {
                     i++;
                 }
                 cout << count << lastString[i] << " ";
-                clusters++;
             }
-
             cout << endl;
-            /*
-            cout << "t-c/t * 100%" << endl;
-            cout << "c = " << clusters << endl;
-            cout << "t = " << sentenceSize + 1 << endl;
-            int ratio;
-            ratio = (sentenceSize - (clusters/sentenceSize+1))/100;
-            cout << "ratio = " << ratio << endl;
-            cout << endl;
-             */
         }
     }
     // if argument = quick
@@ -129,4 +126,3 @@ int main(int argc, char* argv[]) {
 
     return 0;
 }
-// this is a test to see if gitlab updates 
